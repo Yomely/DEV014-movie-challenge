dac6df81328e4b04e09f34e6607e0ee58faa7db5
Yomely <yomelydigital@gmail.com>
2024-06-12 12:51:04 -0500

    Agrega nuevas caracteristicas a los componentes de vista de dettalle y routeado de las peliculas HU2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import Home from './views/Home.js';
 import Detail from './views/Detail.js';
 import Error from './views/Error.js';
+
 // ... import other views
 import { setRootEl, setRoutes, onURLChange } from './router.js';
 
 // Define your routes and their associated views
 const routes = {
   '/': Home,
-  '/detail': Detail,
   '/error': Error,
+  '/detail': Detail,
   // ...
 };
 
--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-undef */
+import { connectDetails } from '../lib/connect';
+
 const Detail = () => {
+  const id = window.location.search.split('=')[1];
   const div = document.createElement('div');
-  div.textContent = 'Soy el detalle de la pelicula';
+  connectDetails(id)
+    .then((res) => {
+      console.log(res);
+      div.textContent = 'Soy el detalle de la pelicula';
+      const template = `
+      <div><h1>${res.title}</h1></div>
+      <img src="https://image.tmdb.org/t/p/w500${res.poster_path}" alt="${res.title}" />
+      <div><p>${res.release_date}</p></div>
+      <div>${res.with_genres}</div>
+      <div>${res.vote_average}</div>
+      `;
+      div.innerHTML = template;
+    })
+    .catch((err) => console.log(err));
   return div;
 };
-
 export default Detail;
